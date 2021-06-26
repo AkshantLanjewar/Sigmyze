b0d89e2a36fc946d25c1afeaa40b2332f3134f57
akshant <akshant>
2021-06-26 13:03:34 -0700

    Fixing by removing build

--- src/index.js
+++ src/index.js
@@ -12,13 +12,26 @@ import { Provider } from 'react-redux'
 
 import ProjectModal from './components/project-components/project-modal'
 
+//react router
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+
 class App extends React.Component {
     render() {
         return (
             <Provider store={store}>
-                <div className="container">
-                    <ProjectDashboard />
-                </div>
+                <Router>
+                    <Route exact path="/">
+                        <div className="container">
+                            <ProjectDashboard />
+                        </div>
+                    </Route>
+
+                    <Route path="/editor">
+                        <div className="container">
+                            
+                        </div>
+                    </Route>
+                </Router>
 
                 <ProjectModal />
             </Provider>
---
+++ src/views/ProjectEditor.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+class ProjectEditor extends React.Component {
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default ProjectEditor
