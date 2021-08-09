74476b04b697a5aad13d51f16821e985724fe9c1
akshant <akshant>
2021-08-09 00:30:12 -0700

    finished map, ready for deployment --- V1 ---

--- src/client/homepage/components/map.tsx
+++ src/client/homepage/components/map.tsx
@@ -8,6 +8,7 @@ import { GeoJsonLayer, PolygonLayer } from '@deck.gl/layers'
 
 import * as d3 from "d3"
 
+
 function Map() {
     const InitialViewState = {
         longitude: -122.41669,
@@ -17,9 +18,13 @@ function Map() {
         bearing: 0
     }
 
-    const [state, setState] = useState({ geoData: {}, scale: null })
+    const [mapdata, setMapData] = useState(null)
+    const [colorscale, setColorScale] = useState({min: 0, max: 0})
 
-    let [geojsonLayer, setgeojsonLayer] = useState(null)
+    function HexToArr(hex: string) {
+        var result: any = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex)
+        return [ parseInt(result[1], 16), parseInt(result[2], 16), parseInt(result[3], 16) ]
+    }
 
     useEffect(() => {
         const url = "/api/data/map/gdp_growth"
@@ -29,26 +34,44 @@ function Map() {
             .then(data => {
                 //create the scale
                 let colorscale = d3.scaleSequential(d3.interpolatePlasma).domain([data["minGrowth"], data["maxGrowth"]])
-                setState({...state, geoData: geojsonLayer["geo"], scale: colorscale})
-                setgeojsonLayer(new GeoJsonLayer({
-                    id: 'gdp_growth_json',
-                    data: state.geoData,
-                    opacity: 0.8,
-                    stroked: false,
-                    filled: true,
-                    extruded: false,
-                    wireframe: true,
-                    getFillColor: f => state.scale(f.properties.growth),
-                    pickable: false
-                }))
+                let geojson = data["geo"]
+
+                setColorScale({min: data["minGrowth"], max: data["maxGrowth"]})
+                setMapData(geojson)
             })
     }, []) 
+
+    const interpolater = d3.scaleSequential(d3.interpolatePlasma).domain([colorscale.min, colorscale.max])
+    const geoLayer = new GeoJsonLayer({
+        id: "geomap",
+        data: mapdata,
+        opacity: 0.8,
+        stroked: false,
+        visible: true,
+        filled: true,
+        extruded: true,
+        wireframe: true,
+        getElevation: (f) => 1000,
+        getFillColor: (f) => {
+            let growth = f.properties.growth
+            if(growth != null)
+                return HexToArr(interpolater(growth))
+            if(growth == null)
+                return [255, 255, 255]
+        },
+        getLineColor: [255, 255, 255],
+        pickable: true
+    })
+
+    console.log(geoLayer)
+
+    const layers = [geoLayer]
 
     return (
         <section className="home-map">
             <DeckGL initialViewState={InitialViewState} 
                     controller={true}
-                    layers={[geojsonLayer]}>
+                    layers={layers}>
                 <StaticMap mapStyle={BASEMAP.DARK_MATTER_NOLABELS} />
             </DeckGL>
         </section>
