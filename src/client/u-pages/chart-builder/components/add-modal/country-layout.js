84e73bf77babd34344766e3a0bc265265f21e176
rahul lanjewar <rahul.lanjewar@gmail.com>
2022-01-06 09:35:27 -0800

    fixing the MyChart data disappearance issue. Used working code from previous commit on Javascriptification

--- src/client/u-pages/chart-builder/components/add-modal/country-layout.js
+++ src/client/u-pages/chart-builder/components/add-modal/country-layout.js
@@ -3,16 +3,16 @@ import { AiOutlineLineChart } from "react-icons/ai"
 
 function SearchIcon() {
     return (
-        <svg xmlns="http://www.w3.org/2000/svg" 
-            width="18" 
-            height="18" 
-            viewBox="0 0 24 24" 
+        <svg xmlns="http://www.w3.org/2000/svg"
+            width="18"
+            height="18"
+            viewBox="0 0 24 24"
             style={{cursor: "pointer"}}
-            fill="none" 
-            stroke="currentColor" 
+            fill="none"
+            stroke="currentColor"
             stroke-width="2"
-            stroke-linecap="round" 
-            stroke-linejoin="round" 
+            stroke-linecap="round"
+            stroke-linejoin="round"
             className="search-icon">
             <circle cx="11" cy="11" r="8"></circle>
             <line x1="21" y1="21" x2="16.65" y2="16.65"></line>
@@ -25,7 +25,7 @@ const stepOneChoices = [{icon: <AiOutlineLineChart />, name: "Internet Not Loadi
 
 function CountryLayout(props) {
     const nextSubmit = props.nextSubmit
-    const setCountry = props.setCountry 
+    const setCountry = props.setCountry
 
     const inputRef = React.createRef()
     const [currentCountryStep, setCurrentCountryStep] = useState(stepOneChoices)
