a38894e6f4c252b88ad5476f51d2b9bc539527ac
akshant <akshant>
2022-02-03 22:52:41 -0800

    started work on dashboard, next would be revamping charting

--- src/client/pages/homepage/views/log.js
+++ src/client/pages/homepage/views/log.js
@@ -1,9 +1,28 @@
 import React from "react"
 
+import { HiPlus } from 'react-icons/hi'
+import Modal from  '../../../components/modal/index'
+
 function LoggedView() {
     return (
-        <div>
+        <div className="main dash-container">
+            <div className="dash-header">
+                <div className="text">
+                    <h1>Welcome to Sigmyze</h1>
+                    <p>Manage your apps, and get a head start with prebuilt templates</p>
+                </div>
 
+                <div className="buttons">
+                    <button>
+                        <HiPlus />
+                        <span>Create App</span>
+                    </button>
+                </div>
+            </div>
+
+            <Modal viewState={false} title={"Create Project"}>
+
+            </Modal>
         </div>
     )
 }
