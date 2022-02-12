aa33a0eca25098e54b9d13225a97a8b46139b317
AkshantLanjewar <52946571+AkshantLanjewar@users.noreply.github.com>
2022-02-12 12:55:50 -0800

    almost doe with datasets, add grid list vie

--- src/client/pages/resources-page/components/chart-card.js
+++ src/client/pages/resources-page/components/chart-card.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import ChartBuilder, { ChartOptions, blueColor, redColor } from "../../../components/chart-builder"
 import * as countries from 'i18n-iso-countries'
 import * as d3 from 'd3'
@@ -7,14 +7,108 @@ countries.registerLocale(require("i18n-iso-countries/langs/en.json"))
 
 function ChartCard(props) {
     let category = props.indicator
-    let iso3     = props.iso3
+    let iso3 = props.iso3
 
     const chartRef = React.createRef()
-    const cardRef  = React.createRef()
+    const cardRef = React.createRef()
+
+    const [simpleName, setSimpleName] = useState("")
+    const [scale, setScale] = useState()
+    const [chartOptions, setChartOptions] = useState({})
+    const [selfDisplay, setSelfDisplay] = useState({})
+    const [cardSize, setCardSize] = useState({})
+
+    useEffect(() => {
+        chartRef.current.innerHTML = ""
+
+        // Check for Covid. Set URL accorindgly //
+
+        let covCheck = category.search(/cc|cd/i)
+        let dataset = ""
+
+        // The below code is stop-gap. Needs a better, scalable logic
+        if (covCheck >= 0) {
+            dataset = 'COVID'
+        }
+        else {
+            dataset = "WEO"
+        }
+
+        let url = `/api/data/v2/datasets/${dataset}/${iso3}/${category}`
+        let xAxType = ""
+
+
+        fetch(url)
+            .then(response => response.json())
+            .then(data => {
+                let chart = new ChartBuilder(chartRef)
+                let chartData = []
+                var lowerI
+
+                if (covCheck >= 0) {
+                    //lowerI = Math.round(data['data'].length*1/3)
+                    lowerI = 0
+                }
+                else {
+                    lowerI = 0
+                }
+
+                for (let i = lowerI; i < data['data'].length; i++) {
+                    let object = {}
+                    let dt = new Date(data['data'][i]["date"])
+
+                    if (covCheck >= 0) {
+                        object["date"] = dt
+                        xAxType = 'D'
+                    }
+                    else {
+                        object["date"] = dt.getUTCFullYear()
+                        xAxType = 'Y'
+                    }
+
+                    object["value"] = data['data'][i]["value"]
+                    chartData.push(object)
+                }
+
+                
+
+                let chartOptions = {
+                    chartType: "line",
+                    chartData: chartData,
+                    chartName: category,
+                    chartColor: blueColor,
+
+                    showXAxis: 1,
+                    showYAxis: 0,
+
+                    formatterPre: `${category}: `,
+
+                    xAxisType: xAxType,
+                    yAxisType: "linear"
+                }
+
+                chart.AddLineChart(chartOptions)
+                chart.CreateChart()
+
+                setSimpleName(data['sName'])
+                setScale(data['units'])
+            })
+    })
 
     return (
-        <div>
+        <div className="card light scaleHov" ref={cardRef} style={{marginTop: "0.5em"}}>
+            <div className="title tooltip">
+                <p className="titleLong" style={{ marginBottom: "0.5em" }}>
+                    <span>{iso3} {'> '}</span>
+                    <span>{simpleName} {'('}{scale}{')'}</span>
+                </p>
 
+                <div className="titleShort">{category}</div>
+
+                <span className="tooltiptext">United States National GDP</span>
+            </div>
+
+            <div className="chart" ref={chartRef}></div>
         </div>
     )
 }
--- src/client/pages/resources-page/dataset.js
+++ src/client/pages/resources-page/dataset.js
@@ -7,38 +7,46 @@ import { AiOutlineLineChart } from 'react-icons/ai'
 
 function DatasetPage() {
     return (
-        <div className="datasets">
-            <div className='header'>
-                <div className='title'>
-                    <h1>World Economic Outlook</h1>
-                    <h5>WEO</h5>
-                </div>
+        <div className="datasets" style={{height: "100%"}}>
+            <div className='inner'>
+                <div className='header'>
+                    <div className='title'>
+                        <h1>World Economic Outlook</h1>
+                        <h5>WEO</h5>
+                    </div>
 
-                <CountrySearch />
+                    <CountrySearch />
 
-                <div className='tab-container' style={{marginTop: "-1em"}}>
-                    <div className='tabs'>
-                        <ul>
-                            <li>
-                                <a className='active'>
-                                    <span><AiOutlineLineChart /></span>
-                                    <span>GDP</span>
-                                </a>
-                            </li>
+                    <div className='tab-container' style={{marginTop: "-1em"}}>
+                        <div className='tabs' style={{ justifySelf: "center" }}>
+                            <ul>
+                                <li>
+                                    <a className='active'>
+                                        <span><AiOutlineLineChart /></span>
+                                        <span>GDP</span>
+                                    </a>
+                                </li>
 
-                            <li>
-                                <a className=''>
-                                    <span><AiOutlineLineChart /></span>
-                                    <span>Trade</span>
-                                </a>
-                            </li>
-                        </ul>
+                                <li>
+                                    <a className=''>
+                                        <span><AiOutlineLineChart /></span>
+                                        <span>Trade</span>
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
                     </div>
                 </div>
-            </div>
 
-            <div className='body'>
-                <ChartCard iso3={"USA"} indicator={"NGDP"} />
+                <div className='body'>
+                    <div className='tab-container' style={{ justifyContent: "flex-end" }}>
+
+                    </div>
+                    
+                    <ChartCard iso3={"USA"} indicator={"NGDP"} />
+                    <ChartCard iso3={"USA"} indicator={"NGDP"} />
+                    <ChartCard iso3={"USA"} indicator={"NGDP"} />
+                </div>
             </div>
         </div>
     )
