87d4ae4509acccef22efc5cd0580f1819cf5a684
akshantlanjewar <akshant.lanjewar@gmail.com>
2022-03-03 23:09:59 -0800

    sync

--- src/client/components/chart-builder/component-adder.js
+++ src/client/components/chart-builder/component-adder.js
@@ -4,12 +4,36 @@ function ComponentModal(props) {
     return (
         <div className="component-sidebar">
             <div className="header">
-                <h5>Add Chart Indicator</h5>
+                <h5>Select Dataset</h5>
                 <button className="close"></button>
             </div>
 
             <div className="body">
+                <div className="dataset">
+                    <div className="inner">
+                        <div className="title">
+                            <img src={"/logos/WEO.svg"} width={"70px"} height={"70px"} />
+                            <h6>World Economic Outlook (WEO)</h6>
+                        </div>
 
+                        <div className="body">
+                            <p>Indicator Description</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="dataset">
+                    <div className="inner">
+                        <div className="title">
+                            <img src={"/logos/WEO.svg"} width={"70px"} height={"70px"} />
+                            <h6>World Economic Outlook (WEO)</h6>
+                        </div>
+
+                        <div className="body">
+                            <p>Indicator Description</p>
+                        </div>
+                    </div>
+                </div>
             </div>
 
             <div className="footer">
