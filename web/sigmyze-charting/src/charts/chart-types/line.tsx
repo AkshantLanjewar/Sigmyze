cb9837fbfcb4d96b7ca545a1f9e359c623ade19f
akshantlanjewar <akshant.lanjewar@gmail.com>
2022-04-25 07:13:45 -0700

    sync ig

--- web/sigmyze-charting/src/charts/chart-types/line.tsx
+++ web/sigmyze-charting/src/charts/chart-types/line.tsx
@@ -1,7 +1,9 @@
 import React from "react"
 
 import { TimeSeriesTimeScale, TimeSeriesLinearScale } from '../utils/chart-axis'
-import { ChartOptions } from '../chart-options'
+import { ChartOptions, ChartData } from '../chart-options'
+
+import * as d3 from 'd3'
 
 interface LineProps {
     options: ChartOptions,
@@ -11,8 +13,26 @@ interface LineProps {
 }
 
 function LineChart({ options, timeScale, linearScale }: LineProps) {
+    function LinePath(x: d3.ScaleTime<number, number, never>, y: d3.ScaleLinear<number, number, never>) {
+        let line = d3.line<ChartData>()
+            .defined(d => !isNaN(d.value))
+            .x(d => x(d.date))
+            .y(d => y(d.value))
+
+        return line
+    }
+
+    const Path = LinePath(timeScale!.x, linearScale!.y)
+
     return (
-        <path />
+        <path
+            fill={"none"}
+            stroke={"#456ef7"}
+            strokeWidth={"2px"}
+            strokeLinejoin="round"
+            strokeLinecap="round"
+            d={}
+        />
     )
 }
 
