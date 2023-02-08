887f1b2ee67103392fa714df4afd0cd0b769c6b3
akshantlanjewar <akshant.lanjewar@gmail.com>
2023-02-07 23:25:33 -0800

    created application layout with updated sidebar params

--- web/ui-next/components/nav-elements/application-layout.tsx
+++ web/ui-next/components/nav-elements/application-layout.tsx
@@ -1,6 +1,65 @@
-const ApplicationLayout: React.FC = ({ }) => {
+import { AppShell, MantineProvider } from "@mantine/core"
+import { NotificationsProvider } from "@mantine/notifications"
+import Head from "next/head"
+import OrganizationContext from "../data/organization/context"
+import UserContext from "../data/user/context"
+import { theme } from "../default-theme"
+import HeaderS from "../static/header"
+import NavbarS from "../static/navbar"
+
+enum ApplicationNavbar {
+    logged_out,
+    logged_in_secure,
+    logged_in_default
+}
+
+interface IApplicationLayoutProps {
+    title: string,
+    description: string,
+    location: string,
+    protectedView: boolean,
+    children: React.ReactNode
+}
+
+const ApplicationLayout: React.FC<IApplicationLayoutProps> = 
+({ title, description, location, protectedView, children }) => {
+    let sidebar = undefined
+    if(protectedView)
+        sidebar = <NavbarS location={location} />
+
+    let header = <HeaderS />
+
     return (
         <>
+            <Head>
+                <title>{title}</title>
+                <meta name="description" content={description} />
+				<meta name="viewport" content="width=device-width, initial-scale=1" />
+            </Head>
+
+            <UserContext>
+                <OrganizationContext>
+                    <MantineProvider 
+                        withGlobalStyles
+                        withNormalizeCSS
+                        withCSSVariables 
+                        theme={theme}
+                    >
+                        <NotificationsProvider>
+                            <AppShell
+                                padding={0}
+                                navbar={sidebar}
+                                header={header}
+                                styles={(theme: any) => ({
+                                    main: { backgroundColor: theme.colors.dark[7] }
+                                })}
+                            >
+                                {children}
+                            </AppShell>
+                        </NotificationsProvider>
+                    </MantineProvider>
+                </OrganizationContext>
+            </UserContext>
         </>
     )
 }
--- web/ui-next/pages/index.tsx
+++ web/ui-next/pages/index.tsx
@@ -1,20 +1,21 @@
-import DefaultLayout from "../components/default-layout"
 import IndexPage from "../components/index"
+import ApplicationLayout from "../components/nav-elements/application-layout"
 import Footer from "../components/nav-elements/footer"
 
 export default function Home() {
 	return (
 		<>
-			<DefaultLayout
+			<ApplicationLayout
 				title="Sigmyze"
 				description=""
 				location="/"
+				protectedView={false}
 			>
 				<div style={{ width: '100%', height: '100%' }}>
 					<IndexPage />
 					<Footer />
 				</div>
-			</DefaultLayout>
+			</ApplicationLayout>
 		</>
 	)
 }
