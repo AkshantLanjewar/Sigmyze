ad5226994b87e63a0165d89a113ec2a98d261cd6
akshantlanjewar <akshant.lanjewar@gmail.com>
2023-02-07 20:19:36 -0800

    created footer and added it to the index page for testing

--- web/ui-next/pages/index.tsx
+++ web/ui-next/pages/index.tsx
@@ -1,5 +1,6 @@
 import DefaultLayout from "../components/default-layout"
 import IndexPage from "../components/index"
+import Footer from "../components/nav-elements/footer"
 
 export default function Home() {
 	return (
@@ -11,6 +12,7 @@ export default function Home() {
 			>
 				<div style={{ width: '100%', height: '100%' }}>
 					<IndexPage />
+					<Footer />
 				</div>
 			</DefaultLayout>
 		</>
