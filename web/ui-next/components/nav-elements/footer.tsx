890f332037a5176e93422c75a9adadb8c9bfe1b1
akshantlanjewar <akshant.lanjewar@gmail.com>
2023-02-07 20:39:00 -0800

    created the logo component

--- web/ui-next/components/nav-elements/footer.tsx
+++ web/ui-next/components/nav-elements/footer.tsx
@@ -1,8 +1,7 @@
-import { Box, Button } from '@mantine/core'
+import { Button } from '@mantine/core'
 import Link from 'next/link'
-import Image from 'next/image'
 import styles from './footer.module.scss'
-import logo  from '../../public/logo.svg'
+import Logo from './logo'
 
 const Footer: React.FC = ({ }) => {
     return (
@@ -22,36 +21,7 @@ const Footer: React.FC = ({ }) => {
             </div>
 
             <div className={styles.links}>
-                <Box 
-                    sx={{ 
-                        display: 'flex', 
-                        flexDirection: 'row', 
-                        alignItems: 'center', 
-                        height: 60,
-                        cursor: 'pointer',
-                        width: 'min-content' 
-                    }}
-                >
-                    <Image 
-                        src={logo} 
-                        height={35} 
-                        alt={"Sigmyze Logo"} 
-                        style={{ marginRight: 5 }}
-                    />
-
-                    <Box 
-                        sx={{
-                            fontSize: 26,
-                            fontWeight: 700,
-                            textAlign: 'center',
-                            fontFamily: '-apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Helvetica,Arial,sans-serif',
-                            display: 'table-cell',
-                            verticalAlign: 'middle'
-                        }}
-                    >
-                        Sigmyze
-                    </Box>
-                </Box>
+                <Logo />
             </div>
         </div>
     )
---
+++ web/ui-next/components/nav-elements/logo.tsx
@@ -0,0 +1,20 @@
+import styles from './logo.module.scss'
+import Image from 'next/image'
+import logo  from '../../public/logo.svg'
+
+const Logo: React.FC = ({ }) => {
+    return (
+        <div className={styles.logo}>
+            <Image 
+                src={logo} 
+                height={35} 
+                alt={"Sigmyze Logo"} 
+                style={{ marginRight: 5 }}
+            />
+
+            <div className={styles.text}>Sigmyze</div>
+        </div>
+    )
+}
+
+export default Logo
