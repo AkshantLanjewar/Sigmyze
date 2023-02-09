74b554ec11fc6efc0a8e67a5eab0994508468b0b
akshantlanjewar <akshant.lanjewar@gmail.com>
2023-02-08 17:01:33 -0800

    finished with features page

--- web/ui-next/pages/features.tsx
+++ web/ui-next/pages/features.tsx
@@ -32,6 +32,45 @@ const FeaturesPage: React.FC = ({ }) => {
                         <div className={`${styles.image} ${styles.chartEditor}`}></div>
                     </div>
 
+                    <div className={`${styles.section} ${styles.alt}`}>
+                        <div className={`${styles.image} ${styles.documentEditor}`}></div>
+
+                        <div className={styles.titleWrapper}>
+                            <div className={styles.sigmyze}>Sigmyze</div>
+                            <div className={styles.title}>
+                                Data
+                                <span className={styles.emphasize}> Presentation</span>
+                            </div>
+
+                            <div className={styles.descriptionWrapper}>
+                                <div className={styles.description}>
+                                    Stop hassling with trying to add charts into your documents, 
+                                    our document editor treats charts as a first class element, making their 
+                                    addition much easier.
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className={`${styles.section} ${styles.tri}`}>
+                        <div className={styles.titleWrapper}>
+                            <div className={styles.sigmyze}>Sigmyze</div>
+                            <div className={styles.title}>
+                                <span className={styles.emphasize}>Cloud </span>
+                                Storage
+                            </div>
+
+                            <div className={styles.descriptionWrapper}>
+                                <div className={styles.description}>
+                                    Don't stress about how to access your data, with Sigmyze, 
+                                    your data is accessible from any Phone, Laptop or PC with an internet connection
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className={`${styles.image} ${styles.driveSs}`}></div>
+                    </div>
+
                     <Footer />
                 </>
             </ApplicationLayout>
