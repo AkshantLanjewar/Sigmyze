dce78429df8fb033d8cbbe1cb52431759ac79d3d
akshantlanjewar <akshant.lanjewar@gmail.com>
2023-02-08 17:59:01 -0800

    created homepage hero

--- web/ui-next/components/index/index.tsx
+++ web/ui-next/components/index/index.tsx
@@ -1,8 +1,11 @@
+import { Button } from "@mantine/core"
+import Link from "next/link"
 import { useRouter } from "next/router"
 import { useContext, useEffect } from "react"
 import { UserContextData } from "../data/user/context"
 import { IUserContext } from "../data/user/types"
 import Footer from "../nav-elements/footer/footer"
+import styles from './home.module.scss'
 
 const IndexPage: React.FC = ({ }) => {
     const { loggedIn } = useContext(UserContextData) as IUserContext
@@ -15,7 +18,47 @@ const IndexPage: React.FC = ({ }) => {
     
     return (
         <div style={{ width: '100%', height: '100%' }}>
-            
+            <div className={styles.hero}>
+                <div className={styles.textContainer}>
+                    <div className={styles.small}>Get Started</div>
+                    <div className={styles.bold}>
+                        Democratizing <span className={styles.focus}>Data</span> and 
+                        <span className={styles.focus}> Analytics</span>
+                    </div>
+
+                    <div className={styles.actions}>
+                        <Link href={"/auth/signup"}>
+                            <Button
+                                variant={"filled"}
+                                color={"indigo"}
+                                size={"lg"}
+                                radius={"xl"}
+                                py={1}
+                            >
+                                Get Started
+                            </Button>
+                        </Link>
+
+                        <Link href={"/features"}>
+                            <Button
+                                variant={"subtle"}
+                                color={"gray"}
+                                size={"lg"}
+                                radius={"xl"}
+                                py={1}
+                            >
+                                Learn More
+                            </Button>
+                        </Link>
+                    </div>
+                </div>
+
+                <div className={styles.imageContainer}>
+                    <div className={styles.image}></div>
+                </div>
+            </div>
+
+
             <Footer />
         </div>
     )
