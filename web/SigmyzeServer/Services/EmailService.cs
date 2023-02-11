ed4373a8aea1d8a4e091581a98ac8ea009f798f5
Akshant Lanjewar <akshant.lanjewar@gmail.com>
2023-02-10 22:25:43 -0800

    merge fix

--- web/SigmyzeServer/Services/EmailService.cs
+++ web/SigmyzeServer/Services/EmailService.cs
@@ -94,43 +94,5 @@ namespace SigmyzeServer.Services
                 }
             }
         }
-
-        public void SendVerificationEmailSES(string token, string address, string? name)
-        {
-            string username = "AKIAYO437S56M4E6BAGL";
-            string password = "BACLLPTVGF1EaHA1X/fPLbQwljV+okbIKptug6tOnQRG";
-            string host = "email-smtp.us-east-1.amazonaws.com";
-            int port = 587;
-
-            string htmlContent = "";
-
-            htmlContent +=  "<div>";
-            htmlContent +=      $"<h1> Hello {name} </h1>";
-            htmlContent +=      $"<h3>Here is your verification token {token} </h3>";
-            htmlContent +=  "</div>";
-
-            MailMessage message = new MailMessage();
-            message.IsBodyHtml = true;
-            message.From = new MailAddress("info@sigmyze.com", "Sigmyze Application");
-            message.To.Add(new MailAddress(address));
-            message.Subject = "Verification Token";
-            message.Body = htmlContent;
-
-            using (var smtpClient = new SmtpClient(host, port))
-            {
-                smtpClient.Credentials = new NetworkCredential(username, password);
-                smtpClient.EnableSsl = true;
-
-                try
-                {
-                    smtpClient.Send(message);
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine("The email was not sent.");
-                    Console.WriteLine("Error message: " + ex.Message);
-                }
-            }
-        }
     }
 }
