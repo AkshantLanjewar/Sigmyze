dcd8e3b2ecad4eacd3c7a0e3b1f3efb63fb3eaa4
akshantlanjewar <akshant.lanjewar@gmail.com>
2023-02-24 00:16:00 -0800

    scaffolded quanta

--- web/ui-next/components/pages/quanta/index.tsx
+++ web/ui-next/components/pages/quanta/index.tsx
@@ -1,5 +1,9 @@
 import QuantaContext from "../../data/quanta/context"
 import ApplicationLayout from "../../nav-elements/application-layout"
+import styles from '../../../pages/lunar/lunar.module.scss'
+import { Group } from "@mantine/core"
+import QuantaToolbar from "../../lunar/toolbar/quanta-toolbar"
+import QuantaViewport from "../../lunar/viewport/quanta-viewport"
 
 const QuantaPage: React.FC = ({ }) => {
     return (
@@ -12,7 +16,20 @@ const QuantaPage: React.FC = ({ }) => {
             >
                 <QuantaContext>
                     <div style={{ height: '100%' }}> 
-                    
+                        <Group
+                            spacing={"xs"}
+                            position={"apart"}
+                            sx={{ height: '100%' }}
+                            noWrap
+                        >
+                            <div className={styles.toolbar}>
+                                <QuantaToolbar />
+                            </div>
+
+                            <div className={styles.viewport}>
+                                <QuantaViewport />
+                            </div>
+                        </Group>
                     </div>
                 </QuantaContext>
             </ApplicationLayout>
