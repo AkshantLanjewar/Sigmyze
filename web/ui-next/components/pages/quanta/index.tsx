22e4a10f15e756f2b77c9e26eec49c30c7b13897
akshantlanjewar <akshant.lanjewar@gmail.com>
2023-02-23 23:49:28 -0800

    added quanta sidebar

--- web/ui-next/components/pages/quanta/index.tsx
+++ web/ui-next/components/pages/quanta/index.tsx
@@ -1,7 +1,18 @@
+import ApplicationLayout from "../../nav-elements/application-layout"
+
 const QuantaPage: React.FC = ({ }) => {
     return (
         <div>
-
+            <ApplicationLayout
+                title="Sigmyze: Quanta Editor"
+                description="The sigmyze Quanta Editor"
+                location="/quanta"
+                protectedView={true}
+            >
+                <div>
+                    
+                </div>
+            </ApplicationLayout>
         </div>
     )
 }
