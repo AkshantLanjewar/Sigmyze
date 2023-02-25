d8ccf0558729208d653c5b6e0cee54c9e8c9fb42
akshantlanjewar <akshant.lanjewar@gmail.com>
2023-02-25 00:42:23 -0800

    crearted selectors, now do tabs

--- web/ui-next/components/quanta/overview-selectors/overview-selectors.tsx
+++ web/ui-next/components/quanta/overview-selectors/overview-selectors.tsx
@@ -1,9 +1,32 @@
+import { ActionIcon, Tooltip } from '@mantine/core'
+import { IconCode, IconCodePlus } from '@tabler/icons'
 import styles from './overview-selectors.module.scss'
 
 const OverviewSelectors: React.FC = ({ }) => {
     return (
-        <div>
+        <div className={styles.selectors__view}>
+            <div className={styles.selector__item}>
+                <ActionIcon className={styles.selector__icon} radius={"md"}>
+                    <IconCode size={48} stroke={"2"} />
+                </ActionIcon>
 
+                <div className={styles.selector__title}>
+                    Selector Name
+                </div>
+            </div>
+
+            <div className={styles.selector__item}>
+                <Tooltip
+                    label={"Create Selector"}
+                    position={"bottom"}
+                    withArrow
+                    styles={{ tooltip: { backgroundColor: "#08090A" } }}
+                >
+                    <ActionIcon className={styles.selector__icon} radius={"md"}>
+                        <IconCodePlus size={48} stroke={"2"} />
+                    </ActionIcon> 
+                </Tooltip>  
+            </div>
         </div>
     )
 }
