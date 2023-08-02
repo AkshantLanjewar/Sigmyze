c2f88118225518103e361a38310966966083be0b
Akshant <akshant.lanjewar@gmail.com>
2023-08-02 14:15:23 -0700

    removing sh

--- web/nginx/setup.sh
+++ web/nginx/setup.sh
@@ -2,6 +2,11 @@
 export AWS_ACCESS_KEY_ID=AKIAYO437S56L2UOZ3MW
 export AWS_SECRET_ACCESS_KEY='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'
 
-
+certbot certonly --non-interactive \
+      --dns-route53 \
+      --preferred-challenge dns \
+      --agree-tos \
+      --domains sigmyze.com,draco.sigmyze.com \
+      --email sigmyze@gmail.com
 
 nginx -g "daemon off;"
