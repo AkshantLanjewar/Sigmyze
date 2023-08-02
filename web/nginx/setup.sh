ed907b49e8fa927db08400cfe282f291f95468ad
Akshant <akshant.lanjewar@gmail.com>
2023-08-02 10:21:02 -0700

    try again

--- web/nginx/setup.sh
+++ web/nginx/setup.sh
@@ -6,7 +6,7 @@ certbot certonly --non-interactive \
       --dns-route53 \
       --preferred-challenge dns \
       --agree-tos \
-      --domains sigmyze.com,lon.sigmyze.com \
+      --domains sigmyze.com,elon.sigmyze.com \
       --email sigmyze@gmail.com
 
 nginx -g "daemon off;"
