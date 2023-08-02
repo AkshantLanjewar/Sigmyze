3749061345430e8d22ed83fcdde0d0fbadf600b5
Akshant <akshant.lanjewar@gmail.com>
2023-08-01 22:48:22 -0700

    blud

--- web/nginx/setup.sh
+++ web/nginx/setup.sh
@@ -6,7 +6,7 @@ certbot certonly --non-interactive \
       --dns-route53 \
       --preferred-challenge dns \
       --agree-tos \
-      --domains sigmyze.com,logn.sigmyze.com \
+      --domains sigmyze.com,lon.sigmyze.com \
       --email sigmyze@gmail.com
 
 nginx -g "daemon off;"
