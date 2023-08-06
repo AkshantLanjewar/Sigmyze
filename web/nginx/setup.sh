d962006cc27fabfd7cf99aea28d0a226d995482c
Akshant <akshant.lanjewar@gmail.com>
2023-08-06 15:13:26 -0700

    debug the sigmyze parser in prod

--- web/nginx/setup.sh
+++ web/nginx/setup.sh
@@ -6,7 +6,7 @@ certbot certonly --non-interactive \
       --dns-route53 \
       --preferred-challenge dns \
       --agree-tos \
-      --domains sigmyze.com,alrightalright.sigmyze.com \
+      --domains sigmyze.com,alrightalrigh.sigmyze.com \
       --email sigmyze@gmail.com
 
 nginx -g "daemon off;"
