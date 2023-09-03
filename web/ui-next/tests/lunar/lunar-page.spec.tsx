e4ada846eed1349346c4f47807af757e43656185
akshantlanjewar <akshant.lanjewar@gmail.com>
2023-09-03 16:15:53 -0700

    BRUH

--- web/ui-next/tests/lunar/lunar-page.spec.tsx
+++ web/ui-next/tests/lunar/lunar-page.spec.tsx
@@ -5,5 +5,23 @@
  * it's button state in order to display the correct set of sidebar buttons
  *  
  * Sidebar Validation tests
- *  - mount test (this test is concerned )
+ *  - mount test (this test is concerned with making sure the folder buttons are shown)
+ *  - folder portal setting test
+ *  - chart portal setting test
+ *  - node portal setting test
+ *  - change portal setting test
+ * 
+ * Folder Buttons Shown 
+ *  - there is a portal section within the toolbar
+ *  - there is one button 
+ *  - button-0 has icon with testId = folder-create
+ *  
+ * Chart Buttons Shown
+ *  - there is a portal section
+ *  - there are 3 buttons
+ *  - button-0 has icon with testId = chart-add
+ *  - button-1 has icon with testId = chart-settings
+ *  - button-2 has icon with testId = chart-remove
+ * 
+ * 
  */
