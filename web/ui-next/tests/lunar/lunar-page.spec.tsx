7dd4acfce5601e6f27ea34c428fcf9ccded6fd93
akshantlanjewar <akshant.lanjewar@gmail.com>
2023-09-03 12:56:03 -0700

    bruh

---
+++ web/ui-next/tests/lunar/lunar-page.spec.tsx
@@ -0,0 +1,9 @@
+/**
+ * this file contains all the testing specs in order to ensure that the Lunar Refresh page matches correctly
+ *  
+ * the first set of tests will be concerned with the validation of the sidebar component within the page, and whether or not it can change
+ * it's button state in order to display the correct set of sidebar buttons
+ *  
+ * Sidebar Validation tests
+ *  - mount test (this test is concerned )
+ */
