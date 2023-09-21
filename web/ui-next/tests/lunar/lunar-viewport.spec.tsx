3d628873aaca556b83e166ac29b2f12751fe317b
akshantlanjewar <akshant.lanjewar@gmail.com>
2023-09-20 21:57:23 -0700

    finished creating test

--- web/ui-next/tests/lunar/lunar-viewport.spec.tsx
+++ web/ui-next/tests/lunar/lunar-viewport.spec.tsx
@@ -201,6 +201,11 @@ const deleteWarningLocator = "delete-warning"
  */
 const confirmCheckboxLocator = "confirm-checkbox"
 
+/**
+ * this is the locator for the close button within a tab
+ */
+const closeTabLocator = "close-tab"
+
 test('Lunar Refresh Viewport + Folder Deletion integration test', async ({ mount, page }) => {
     const component = await mount (
         <MemoryRouterProvider url={'/lunar'}>
@@ -260,5 +265,179 @@ test('Lunar Refresh Viewport + Folder Deletion integration test', async ({ mount
     await expect(dummyFolder).toContainText("Dummy Folder")
 
     //now we want to click on the dummy folder
-    
+    await dummyFolder.locator('button').first().click()
+
+    //now we to click buttonZero again
+    await buttonZero.click()
+
+    //now we need to get the new note button and click it
+    const newNoteButton = page.getByTestId(newNoteButtonLocator)
+    await newNoteButton.click()
+
+    //now we need to check note-name = New Note Name
+    const noteNameInput = page.getByTestId(noteNameInputLocator)
+    await expect(noteNameInput).toContainText("New Note Name")
+
+    //now we need to type in dummy note into the input
+    const noteNameRaw = noteNameInput.locator('input')
+    await noteNameRaw.type("Dummy Note", { delay: 200 })
+
+    //now we need to submit the form
+    await submitButton.click()
+
+    //now we need to check the Dummy Folder has one child
+    const dummyFolderChildren = dummyFolder.getByTestId(folderChildrenLocator)
+    await expect(dummyFolderChildren.locator('> button')).toHaveCount(1)
+
+    //now we need to check the dummy childs child = dummy note
+    const dummyNoteLocator = addExtensions(containerElementBase, ["0"])
+    const dummyNote = dummyFolderChildren.getByTestId(dummyNoteLocator)
+    await expect(dummyNote).toContainText("Dummy Note")
+
+    //click on dummyFolder again
+    await dummyFolder.locator('button').first().click()
+
+    //now we need to click on buttonZero again
+    await buttonZero.click()
+
+    //now we need to get the new chart button and click it
+    const newChartButton = page.getByTestId(newChartButtonLocator)
+    await newChartButton.click()
+
+    //now we need to check chart-name = New Chart Name
+    const chartNameInput = page.getByTestId(chartNameInputLocator)
+    await expect(chartNameInput).toContainText("New Chart Name")
+
+    //now we need to type in dummy chart into the input
+    const chartNameRaw = chartNameInput.locator('input')
+    await chartNameRaw.type('Dummy Chart', { delay: 200 })
+
+    //now we need to submit the form
+    await submitButton.click()
+
+    //now we need to see that the dummy folder children has 2 children
+    await expect(dummyFolderChildren.locator('> button')).toHaveCount(2)
+
+    //now we need to check that element-1 = Dummy Chart
+    const dummyChartLocator = addExtensions(containerElementBase, ["1"])
+    const dummyChart = dummyFolderChildren.locator(dummyChartLocator)
+    await expect(dummyChart).toContainText("Dummy Chart")
+
+    //now we need to check viewport-tabs are attached
+    const viewportTabs = component.getByTestId(viewportTabsLocator)
+    await expect(viewportTabs).toBeAttached()
+
+    //we want to check the viewport-viewport is attached
+    const viewportViewport = component.getByTestId(viewportViewportLocator)
+    await expect(viewportViewport).toBeAttached()
+
+    //now we need to check that the viewport-tabs has 2 children
+    await expect(viewportTabs.locator('> button')).toHaveCount(2)
+
+    //we want to check that viewport-tab-0 = Dummy Note
+    const dummyNoteTabLocator = addExtensions(viewportTabBase, ["0"])
+    const dummyNoteTab = viewportTabs.getByTestId(dummyNoteTabLocator)
+    await expect(dummyNoteTab).toContainText("Dummy Note")
+
+    //we want to check that viewport-tab-1 = Dummy Chart
+    const dummyChartTabLocator = addExtensions(viewportTabBase, ["1"])
+    const dummyChartTab = viewportTabs.getByTestId(dummyChartTabLocator)
+    await expect(dummyChartTab).toContainText("Dummy Chart")
+
+    //we want to check that dummyChartTab is active
+    const dummyChartTabValue = await dummyChartTab.getAttribute('data-testValue')
+    await expect(dummyChartTabValue).toBe("active")
+
+    //now we want to get the viewportViewport testValue
+    const viewportViewportValueChart = await viewportViewport.getAttribute('data-testValue')
+    await expect(viewportViewportValueChart).toBe("chart")
+
+    //now we want to click on the note tab
+    await dummyNoteTab.click()
+
+    //now we want to check that the dummyNoteTab is active
+    const dummyNoteTabValue = await dummyNoteTab.getAttribute('data-testValue')
+    await expect(dummyNoteTabValue).toBe("active")
+
+    //we want to check the viewportviewport = note
+    const viewportViewportValueNote = await viewportViewport.getAttribute('data-testValue')
+    await expect(viewportViewportValueNote).toBe("note")
+
+    //now we click on folder-0 again
+    await projectFolder.locator('button').first().click()
+
+    //now we want to check that the delete button is disabled
+    const deleteFolderButtonLocator = addExtensions(buttonBase, ["1"])
+    const deleteFolderButton = component.getByTestId(deleteFolderButtonLocator)
+    await expect(deleteFolderButton).toHaveAttribute("disabled", "true")
+
+    //now we want to click on the dummyFolder
+    await dummyFolder.locator('button').first().click()
+
+    //now the delete button should not be disabled
+    await expect(deleteFolderButton).not.toHaveAttribute("disabled", "true")
+
+    //now we want to click on the deleteButton
+    await deleteFolderButton.click()
+
+    //now we want to check the delete-warning element is attached
+    const deleteWarning = page.getByTestId(deleteWarningLocator)
+    await expect(deleteWarning).toBeAttached()
+
+    //now we want to check the confirm-checkbox element is attached
+    const confirmCheckbox = page.getByTestId(confirmCheckboxLocator)
+    await expect(confirmCheckbox).toBeAttached()
+
+    //now we need to check the submit button is disabled
+    await expect(submitButton).toHaveAttribute("disabled", "true")
+
+    //now we want to check the checkbox
+    const confirmCheckboxRaw = confirmCheckbox.locator('input')
+    await confirmCheckboxRaw.click()
+
+    //now we need to check the submit button is not disabled and click it
+    await expect(submitButton).not.toHaveAttribute("disabled", "true")
+    await submitButton.click()
+
+    //folder-0 has 0 children
+    await expect(projectFolderChildren.locator('> div')).toHaveCount(0)
+    await expect(projectFolderChildren.locator('> button')).toHaveCount(0)
+
+    //viewport tabs has 0 children
+    await expect(viewportTabs.locator('> button')).toHaveCount(2)
+
+    //we need to check if viewportViewport is undefined
+    const viewportViewportValueUndefined = await viewportViewport.getAttribute('data-testValue')
+    await expect(viewportViewportValueUndefined).toBe("undefined")
+
+    //now we click on folder-0 again
+    await projectFolder.locator('button').first().click()
+
+    //click new note button
+    await newNoteButton.click()
+
+    //now we want to type in Dummy Note 
+    await noteNameRaw.type('Dummy Note', { delay: 200 })
+
+    //now we want to submit the form
+    await submitButton.click()
+
+    //now we want to check that viewport-tabs has 1 child
+    await expect(viewportTabs.locator('> button')).toHaveCount(1)
+
+    //we want to check viewport-tab-0 is active
+    const dummyNoteTabValueTest = await dummyNoteTab.getAttribute('data-testValue')
+    await expect(dummyNoteTabValueTest).toBe("active")
+
+    //check if viewportviewport matches
+    const viewportNoteMatchTest = await viewportViewport.getAttribute('data-testValue')
+    await expect(viewportNoteMatchTest).toBe("note")
+
+    //now we want to close the dummy note
+    const dummyNoteTabClose = dummyNoteTab.locator(closeTabLocator)
+    await dummyNoteTabClose.click()
+
+    //now viewport-viewport should be undefined
+    const viewportNullCheck = await viewportViewport.getAttribute('data-testValue')
+    await expect(viewportNullCheck).toBe("undefined")
 })
